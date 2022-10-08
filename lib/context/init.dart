a35f9952551db8469d9e0ce9e4b20ed5a479fffc
Fatbrother <kevin92422@gmail.com>
2022-10-09 04:12:58 +0800

    add screenshot disable function

--- lib/context/init.dart
+++ lib/context/init.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'firebase_options.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 
 Future<void> initialize() async {
   WidgetsFlutterBinding.ensureInitialized();
   var platform = DefaultFirebaseOptions.currentPlatform;
   await Firebase.initializeApp(options: platform);
+  // close the function of screen shot
+  await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
 }
