1b93cf38d838011a60e0758c18aca065c0491071
Fatbrother <kevin92422@gmail.com>
2022-10-14 22:52:30 +0800

    update design

--- lib/context/design.dart
+++ lib/context/design.dart
@@ -4,11 +4,18 @@ import 'package:flutter/material.dart';
 // use this class to define all the details of our design
 // for example, the color of the button, the color of the text, border radius, etc.
 class Design {
-  static const Color _primaryColor = Colors.blue;
-  static const Color _secondaryColor = Color(0xFF2E2E2E);
-  static const Color _tertiaryColor = Color(0xFF3E3E3E);
+  static const Color primaryColor = Color(0xFF4F809B);
+  static const Color secondaryColor = Color(0x754F809B);
+  static const Color backgroundColor = Color(0xFFD9D9D9);
 
-  static Color get primaryColor => _primaryColor;
-  static Color get secondaryColor => _secondaryColor;
-  static Color get tertiaryColor => _tertiaryColor;
+  static const Color primaryTextColor = Colors.black;
+  static const Color secondaryTextColor = Color.fromARGB(137, 11, 11, 11);
+
+  static const outsideUpSpacing = 40.0;
+  static const outsideDownSpacing = 20.0;
+  static const outsideLeftSpacing = 20.0;
+  static const outsideRightSpacing = 20.0;
+  static const insideSpacing = 10.0;
+  static const outsideBorderRadius = 20.0;
+  static const insideBorderRadius = 40.0;
 }
