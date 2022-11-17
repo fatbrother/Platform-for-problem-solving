f46cc530993c57410d5d362b2b53c4efe443d31d
Fatbrother <kevin92422@gmail.com>
2022-11-17 15:56:03 +0800

    fix rating result page

--- lib/frontEnd/pages/rating_result_page.dart
+++ lib/frontEnd/pages/rating_result_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:pops/frontEnd/design.dart';
+import 'package:pops/frontEnd/widgets/app_bar.dart';
 import 'package:pops/frontEnd/widgets/buttons.dart';
 import 'package:pops/frontEnd/widgets/suggest_field.dart';
+
 
 class RatingResultPage extends StatefulWidget {
   const RatingResultPage({super.key});
@@ -18,23 +20,13 @@ class _RatingResultPageState extends State<RatingResultPage> {
     return GestureDetector(
       onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
       child: Scaffold(
+        appBar: MyAppBar.titleAppBar(title: "檢舉原因"),
         backgroundColor: Design.backgroundColor,
         body: Container(
           margin: Design.spacing,
-          //padding: Design.spacing,
           child: SingleChildScrollView(
             child: Column(
               children: [
-                SizedBox(height: Design.getScreenHeight(context) * 0.05),
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () => {},
-                      icon: const Icon(Icons.arrow_back),
-                      iconSize: 35,
-                    ),
-                  ],
-                ),
                 const Text(
                   '請提出檢舉原因',
                   style: TextStyle(
@@ -104,12 +96,12 @@ class _RatingResultPageState extends State<RatingResultPage> {
                   ),
                 ),
                 ReportField(
-                  maxline: 18,
+                  maxline: 12,
                   hintTextFloating: '請對檢舉原因加以解釋...',
                   controller: ratingController,
                 ),
                 SizedBox(height: Design.getScreenHeight(context) * 0.02),
-                SendButton(onPressed: () => {}, text: '送出'),
+                SendButton(onPressed: () {}, text: '送出'),
               ],
             ),
           ),
