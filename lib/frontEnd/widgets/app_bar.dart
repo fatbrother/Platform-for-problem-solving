84e661d4d99a66bcf2b09e9d9b49fc7f575cfeff
Fatbrother <kevin92422@gmail.com>
2022-11-19 22:22:00 +0800

    change app bar style

--- lib/frontEnd/widgets/app_bar.dart
+++ lib/frontEnd/widgets/app_bar.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:pops/frontEnd/routes.dart';
+import 'package:pops/frontEnd/design.dart';
 
-class MyAppBar {
-  static AppBar titleAppBar({required String title}) {
-    return AppBar(
-      //build arrow_back
-      leading: Builder(
-        builder: (BuildContext context) {
-          return IconButton(
-            icon: const Icon(Icons.arrow_back),
-            color: const Color.fromARGB(255, 0, 0, 0),
-            onPressed: () => Routes.back(context),
-          );
-        },
-      ),
-      //AppBar color and word
-      backgroundColor: const Color.fromARGB(222, 255, 255, 255),
-      title: Text(title, style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0))),
-      centerTitle: true,
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: Design.getScreenHeight(context) * 0.04),
+        Row(
+          children: [
+            IconButton(
+              onPressed: () => {},
+              icon: const Icon(Icons.arrow_back),
+              iconSize: 35,
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
